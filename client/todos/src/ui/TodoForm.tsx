85251567ecd0e4289da972761db7c62ef0fdc5b2
reinelic <reine.iramurikiye@clearglobal.org>
2024-03-20 11:36:09 +0200

    set up the form

--- client/todos/src/ui/TodoForm.tsx
+++ client/todos/src/ui/TodoForm.tsx
@@ -1,9 +1,14 @@
 import { useForm, SubmitHandler } from 'react-hook-form'
 import MaxWidthWrapper from './MaxWidthWrapper'
+import { Input } from '@/components/ui/input'
+import { Button } from '../components/ui/button'
 
-type status = 'COMPLETED' | 'TODO' | 'IN_PROGRESS'
+type Status = 'COMPLETED' | 'TODO' | 'IN_PROGRESS'
 type Todo = {
+  title: string
   description: string
+  deadline: Date
+  status: Status
 }
 
 const TodoForm = () => {
@@ -19,16 +24,41 @@ const TodoForm = () => {
     <MaxWidthWrapper>
       {' '}
       <main className=' flex min-h-screen flex-col justify-center '>
-        <div className='flex  flex-grow flex-col'>
+        <div className='mt-8 flex flex-grow flex-col items-center'>
+          <h1 className='mb-4 '> Please enter your tasks below </h1>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <input
+            <Input
+              {...register('title', { required: true })}
+              placeholder='Enter title of task'
+              className='mb-3'
+            />
+            {errors.title && <span>This field is required</span>}
+
+            <Input
               {...register('description', { required: true })}
-              placeholder='enter your todo'
+              placeholder='Enter title of task'
+              className='mb-3'
             />
 
             {errors.description && <span>This field is required</span>}
 
-            <input type='submit' />
+            <Input
+              {...register('deadline', { required: true })}
+              placeholder='Enter deadline'
+              className='mb-3'
+              type='date'
+            />
+            {errors.deadline && <span>This field is required</span>}
+
+            <Input
+              {...register('status', { required: true })}
+              placeholder='Enter description of task'
+              className='mb-3'
+            />
+
+            {errors.status && <span>This field is required</span>}
+
+            <Button> Add Task </Button>
           </form>
         </div>
       </main>
